31df6f93d0c401a36e7df9a75ce8f8327ba1d557
Ivan Kovachev <ivann9963@gmail.com>
2025-08-04 12:08:13 +0300

    unpack Spring Boot starter

---
+++ springtasker/src/main/java/com/example/springtasker/SpringtaskerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtaskerApplication.class, args);
+	}
+
+}
---
+++ springtasker/src/test/java/com/example/springtasker/SpringtaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
